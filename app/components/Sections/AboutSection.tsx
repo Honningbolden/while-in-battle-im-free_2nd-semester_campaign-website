c76fec61ed57055e6cf58acc1160e9dd377e269e
Honningbolden <hannibalmunch@gmail.com>
2024-06-21 14:47:34 +0200

    refactor: Update AboutSection to use SeekingNavItem component and add motion animations
    - Replace NavItem component with SeekingNavItem component in AboutSection
    - Add motion animations for the heading and paragraph elements in ticketInformation component
    - Adjust footer height and background in StickyFooter component
    - Update package.json dependencies and add lazy loading for images
    - Fix information and sponsor overlay dimensions for Firefox and Chrome in StickyFooter component

--- app/components/Sections/AboutSection.tsx
+++ app/components/Sections/AboutSection.tsx
@@ -33,6 +33,8 @@ export default function AboutSection({ aboutContent }: AboutSectionProps) {
   const toggleOpen = () => setIsOpen(!isOpen);
   const [selectedTab, setSelectedTab] = useState(aboutContent[0]);
 
+  const [menuIsOpen, setMenuIsOpen] = useState(true);
+
   const toggleSection = (section: Section) => {
     setSelectedTab(section);
   }
@@ -41,7 +43,7 @@ export default function AboutSection({ aboutContent }: AboutSectionProps) {
     initial: {
       height: 0,
       width: 0,
-      backgroundColor: "#0024cc",
+      backgroundColor: "#ffffff",
       boxShadow: "inset 0 0 0 30px #000000",
       borderRadius: 50,
     },
@@ -63,23 +65,20 @@ export default function AboutSection({ aboutContent }: AboutSectionProps) {
       <InformationOverlay toggleOpen={toggleOpen} />
       <AnimatePresence>
         {isOpen && (
-        <div
-          className={`${styles.clippy} fixed z-[500] top-0 left-0 overflow-hidden`}>
-          <motion.div className='relative h-screen w-screen flex flex-row bg-white overflow-y-scroll'
-            initial={{ y: 0.8, opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ opacity: 0 }}>
-            <div>
-
-              <ul id='nav' className='absolute top-0 z-50 h-full flex flex-col items-start justify-center gap-8'>
+          <div
+            className={`${styles.clippy} fixed z-[500] top-0 min-h-dvh w-dvw left-0 overflow-hidden`}>
+            <motion.div className='relative min-h-dvh w-full flex flex-col md:flex-row bg-white overflow-y-scroll' initial={{ y: 0.8, opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ opacity: 0 }}>
+              <ul id='nav' className='md:relative h-screen top-0 z-50 flex flex-col items-center justify-center gap-4'>
                 {aboutContent.map((section, index) => (
                   <NavItem
-                  key={index}
-                  callback={() => toggleSection(section)}
-                  active={section === selectedTab}
-                  label={section.title}
+                    key={index}
+                    callback={() => toggleSection(section)}
+                    active={section === selectedTab}
+                    label={section.title}
                   />
                 ))}
               </ul>
-              <motion.div className="absolute top-0 left-0 m-8 z-50"
+              <motion.div className="absolute top-0 left-0 m-2 md:m-8 z-50"
                 onClick={toggleOpen}
                 variants={closeButtonVariants}
                 initial="initial"
@@ -88,39 +87,41 @@ export default function AboutSection({ aboutContent }: AboutSectionProps) {
                 whileTap="tap"
                 transition={{ type: "spring", damping: 20, stiffness: 600 }}
               />
-            </div>
-            <div id="content" className="flex flex-col justify-center items-center w-full">
-              <AnimatePresence mode="wait">
-                <motion.div
-                  key={selectedTab ? selectedTab.title : "empty"}
-                  initial={{ y: 10, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  exit={{ y: -10, opacity: 0 }}
-                  transition={{ duration: 0.2 }}
-                  className="w-1/2 flex flex-col gap-12"
-                >
-                  <div className="flex flex-row gap-20">
-                    <div className="flex flex-col w-full">
-                      {selectedTab ? selectedTab.articles.map((article, articleIndex) => (
-                        <div key={article.title} className="flex flex-col gap-2">
-                          <h1 className="title-type text-4xl font-bold mb-4 mt-8"
-                          >{article.title}</h1>
-                          {article.paragraphs.map((paragraph, pIndex) => (
-                            <p key={`${articleIndex}-${pIndex}`} className="body-text text-xl font-light">{paragraph}</p>
-                          ))}
-                        </div>
-                      )) : null}
+              <div id="content" className="flex flex-col justify-start items-center w-full">
+                <AnimatePresence mode="wait">
+                  <motion.div
+                    key={selectedTab ? selectedTab.title : "empty"}
+                    initial={{ y: 10, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: -10, opacity: 0 }}
+                    transition={{ duration: 0.2 }}
+                    className="min-h-dvh w-full p-4 md:p-16 flex flex-col md:justify-center items-start gap-12"
+                  >
+                    <div className="flex flex-col xl:flex-row justify-center items-center gap-16">
+                      <div className="flex flex-col w-full lg:w-1/2 2xl:w-1/3">
+                        {selectedTab ? selectedTab.articles.map((article, articleIndex) => (
+                          <div key={article.title} className="flex flex-col 2xl:max-w-screen-md gap-2">
+                            <h1 className="title-type text-4xl font-bold mb-4 md:mt-8"
+                            >{article.title}</h1>
+                            <div className="w-full">
+                              {selectedTab.imgUrl &&
+                                <div className="2xl:float-end mb-4 w-full md:flex justify-center hidden">
+                                  <img src={selectedTab.imgUrl} className=" ~@xl/3xl:~max-w-64/96 object-cover" />
+                                </div>
+                              }
+                              {article.paragraphs.map((paragraph, pIndex) => (
+                                <p key={`${articleIndex}-${pIndex}`} className="body-text text-xl font-light text-pretty">{paragraph}</p>
+                              ))}
+                            </div>
+                          </div>
+                        )) : null}
+                      </div>
                     </div>
-                    {selectedTab.imgUrl ?
-                      <div className="h-full w-2/3">
-                        <img src={selectedTab.imgUrl} className="h-full w-full object-cover" />
-                      </div> : null}
-                  </div>
-                </motion.div>
-              </AnimatePresence>
-            </div>
-          </motion.div >
-        </div>
+                  </motion.div>
+                </AnimatePresence>
+              </div>
+            </motion.div >
+          </div>
         )}
       </AnimatePresence >
     </>
--- app/components/SeekingNavItem.tsx
+++ app/components/SeekingNavItem.tsx
@@ -39,7 +39,7 @@ export default function SeekingNavItem({ callback, active, maxDistance = 50, lab
       whileHover="hover"
       animate={{ backgroundColor: active ? "#ffffff00" : "#0024cc", color: active ? "#000000" : "#ffffff", scale: active ? 1 : 0.7 }}
       transition={{ type: "spring", damping: 20, stiffness: 400 }}
-      className={`cursor-pointer size-48 rounded-full flex justify-center items-center text-center`}
+      className={`cursor-pointer size-64 md:size-48 rounded-full flex justify-center items-center text-center`}
       onClick={callback}>
       <h1 className="button-text font-bold text-3xl">
         {label}
