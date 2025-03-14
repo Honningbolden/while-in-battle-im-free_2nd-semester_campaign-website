edd508c2d0b158a8158790380788d8f199303b5e
Honningbolden <hannibalmunch@gmail.com>
2025-03-14 11:11:57 -0700

    refactor: Simplify content fetching in page.tsx and improve error handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,33 @@
-import HeroSection from "./components/Sections/heroSection";
-import TicketInformation from "./components/Sections/ticketInformation";
-import TeaserDescription from "./components/Sections/teaserDescription";
-import AboutSection from "./components/Sections/AboutSection";
-import ParallaxBackground from "./components/ParallaxBackground";
-import StickyFooter from "./components/StickyFooter";
+import HeroSection from './components/Sections/heroSection';
+import TicketInformation from './components/Sections/ticketInformation';
+import TeaserDescription from './components/Sections/teaserDescription';
+import AboutSection from './components/Sections/AboutSection';
+import ParallaxBackground from './components/ParallaxBackground';
+import StickyFooter from './components/StickyFooter';
 
 async function fetchContentJSON() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const res = await fetch(`${baseUrl}/cms/content.json`, { cache: 'no-store' });
-  const data = await res.json();
-  if (!res.ok) {
+  try {
+    const res = await fetch(`/cms/content.json`, { cache: 'no-store' });
+    if (!res.ok) {
+      throw new Error('Failed to fetch content');
+    }
+    return await res.json();
+  } catch (error) {
+    console.error('Failed to fetch content:', error);
     throw new Error('Failed to fetch content');
   }
-
-  return data;
 }
 
 export default async function Home() {
   const { ticketInformation, aboutContent } = await fetchContentJSON();
 
-
   return (
     <>
-      <main className="relative overflow-x-clip">
-        <ParallaxBackground/>
+      <main className='relative overflow-x-clip'>
+        <ParallaxBackground />
         <AboutSection aboutContent={aboutContent} />
         <HeroSection />
-        <StickyFooter contentData={ticketInformation}/>
+        <StickyFooter contentData={ticketInformation} />
       </main>
     </>
   );
